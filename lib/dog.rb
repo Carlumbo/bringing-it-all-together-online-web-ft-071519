30bfea3ac07ade156abec494b510d6f10be345eb
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-26 20:12:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -90,7 +90,9 @@ class Dog
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else 
       dog = self.create(name: name, breed: breed )
-      
+    end 
+    dog 
+  end 
       
     
   
